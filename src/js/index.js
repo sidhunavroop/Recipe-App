781a22c026b0ff7ad115889d9e0b6c1733b00e19
Navroop Singh Sidhu <sidhunavroop@yahoo.com>
2019-06-26 01:05:58 -0400

    Making first API call

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,14 @@
-// Global app controller
-import x from './test';
-const num = 15;
-console.log(`I imported and now trying ${x}. Variable num is = ${num}`);
+import axios from 'axios';
+async function getResults(query) {
+  const key = '00961cd13d5ff593655d4e35250dbdd9';
+  const proxy = `https://cors-anywhere.herokuapp.com/`;
+  try {
+    const res = await axios(`${proxy}https://www.food2fork.com/api/search?key=${key}&q=${query}`);
+    const recipes = res.data.recipes;
+    console.log(recipes);
+  } catch(error) {
+      alert(error);
+  }
+  
+}
+getResults('penne pasta');
